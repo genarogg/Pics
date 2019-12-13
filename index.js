af7a9ce837c9e2476fa0ec8268a79dddc0118b5f
ganarogg <gena_gonzalez15@hotmail.com>
2019-12-13 14:10:19 -0400

    Comentarios

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 "use strict"
 
+/* Instanciado los objetos app y BrowserWindow */
 const { app, BrowserWindow } = require("electron");
 
-
-
+/* Imprimiendo un mensaje en la consola antes de salir */
 app.on("before-quit", () => {
     console.log("Saliendo...");
 });
 
+/* Ejecutando ordenes cuando la app esta lista */
 app.on("ready", () =>{
+    /* Crea la ventana */
     let win = new BrowserWindow();
 
+    /* Detecta el cierre de la ventana para cerrar el aplicativo */
     win.on("closed", () => {
         win = null,
         app.quit()
