a1e6c24f2a1027340ca0d556838378d848f94c78
ganarogg <gena_gonzalez15@hotmail.com>
2019-12-13 13:49:15 -0400

    BrowserWindow

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 "use strict"
 
-const { app } = require("electron");
+const { app, BrowserWindow } = require("electron");
 
-console.dir(app)
+
 
 app.on("before-quit", () => {
     console.log("Saliendo...");
 });
 
-app.quit();
+app.on("ready", () =>{
+    let win = new BrowserWindow();
+
+    win.on("closed", () => {
+        win = null,
+        app.quit()
+    })
+})
