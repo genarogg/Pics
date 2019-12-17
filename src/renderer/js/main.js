a615ffb5c45efa72999457bb05fa180d7901a0d7
genarogg <gena_gonzalez15@hotmail.com>
2019-12-17 14:07:32 -0400

    addImagesEvents > Funciones para seleccionar la imagen

--- src/renderer/js/main.js
+++ src/renderer/js/main.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", () => {
-    
+    addImagesEvents()
 });
+
+function addImagesEvents(){
+    const thumbs = document.querySelectorAll("li.list-group-item");
+
+    for(let i = 0, lengthl = thumbs.length; i < lengthl; i++){
+        thumbs[i].addEventListener("click", function() {
+            changeImage(this)
+        })
+    }
+
+    
+}
+
+function changeImage (node) {
+
+    document.querySelector("li.selected").classList.remove("selected");
+
+    node.classList.add("selected")
+    
+    document.getElementById("image-displayed").src = node.querySelector("img").src
+}
+
