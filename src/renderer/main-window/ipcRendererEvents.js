fa0e1b397a2e90ea5f84994b0ddafb488554990e
Genarogg <genarrogg@gmail.com>
2020-02-20 23:59:26 -0400

    subida delas imagenes

--- src/renderer/main-window/ipcRendererEvents.js
+++ src/renderer/main-window/ipcRendererEvents.js
@@ -30,6 +30,7 @@ function setIpc() {
     saveImage(file, err => {
       if (err) return showDialog("error", "Platzipics", err.message);
 
+      document.getElementById("image-displayed").dataset.filtered = file
       showDialog("info", "Platzipics", "La imagen fue guardada");
     });
   });
@@ -78,51 +79,53 @@ function saveFile() {
   ipcRenderer.send("open-save-dialog", ext);
 }
 
-function uploadImage() {
-  let image = document.getElementById("image-displayed").src;
-  image = image.replace("file://");
-  let fileName = path.basename(image);
+function uploadImage () {
+  let imageNode = document.getElementById('image-displayed')
+  let image
+  if (imageNode.dataset.filtered) {
+    image = imageNode.dataset.filtered
+  } else {
+    image = imageNode.src
+  }
 
-  if (settings.has("cloudup.user") && settings.has("cloudup.passwd")) {
-    const decipher = crypto.createDecipher("aes192", "Platzipics");
-    let decrypted = decipher.update(
-      settings.get("cloudup.passwd"),
-      "hex",
-      "utf8"
-    );
-    decrypted += decipher.final("utf8");
+  image = image.replace('plp://', '')
+  let fileName = path.basename(image)
+
+  if (settings.has('cloudup.user') && settings.has('cloudup.passwd')) {
+    document.getElementById('overlay').classList.toggle('hidden')
+
+    const decipher = crypto.createDecipher('aes192', 'Platzipics')
+    let decrypted = decipher.update(settings.get('cloudup.passwd'), 'hex', 'utf8')
+    decrypted += decipher.final('utf8')
 
     const client = Cloudup({
-      user: settings.get("cloudup.user"),
+      user: settings.get('cloudup.user'),
       pass: decrypted
-    });
+    })
 
-    const stream = client.stream({ title: `Platzipics - ${fileName}` });
-    stream.file(image).save((error) =>{
-      if(error){
-        showDialog(
-          "error",
-          "Platzi",
-          "Verifique su coneccion o credenciales"
-        ); 
-      }
+    const stream = client.stream({ title: `Platzipics - ${fileName}` })
+    stream.file(image).save((err) => {
+      document.getElementById('overlay').classList.toggle('hidden')
+      if (err) {
+        showDialog('error', 'Platzipics', 'Verifique su conexión y/o sus credenciales de Cloudup')
+      } else {
+        clipboard.writeText(stream.url)
+        const notify = new Notification('Platzipics', { //eslint-disable-line
+          body: `Imagen cargada con éxito - ${stream.url}, el enlace se copio al portapeles ` +
+                `De click para abrir la url`,
+          silent: false
+        })
 
-        else{
-          showDialog(
-            "error",
-            "Platzi",
-            `Imagen cargada con exito - ${stream.url}`
-          );
+        notify.onclick = () => {
+          shell.openExternal(stream.url)
         }
+      }
     })
   } else {
-    showDialog(
-      "error",
-      "Platzi",
-      "Por favor complete las preferencias de cloudup."
-    );
+    showDialog('error', 'Platzipics', 'Por favor complete las preferencias de Cloudup')
   }
 }
+
 
 module.exports = {
   setIpc: setIpc,
