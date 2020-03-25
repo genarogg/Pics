fbb2a72eac8002df4bf4efe4b54ac1d5851d4c07
Genarogg <genarrogg@gmail.com>
2020-03-25 11:57:49 -0400

    img

--- src/renderer/main-window/filters.js
+++ src/renderer/main-window/filters.js
@@ -1,27 +1,28 @@
-import fs from 'fs-extra'
+import fs from "fs-extra";
 
-function applyFilter (filter, currentImage) {
-  let imgObj = new Image() // eslint-disable-line
-  imgObj.src = currentImage.dataset.original
+function applyFilter(filter, currentImage) {
+  let imgObj = new Image(); // eslint-disable-line
+  imgObj.src = currentImage.dataset.original;
 
-  filterous.importImage(imgObj, {}) // eslint-disable-line
+  filterous
+    .importImage(imgObj, {}) // eslint-disable-line
     .applyInstaFilter(filter)
-    .renderHtml(currentImage)
+    .renderHtml(currentImage);
 }
 
-function saveImage (fileName, callback) {
-  let fileSrc = document.getElementById('image-displayed').src
+function saveImage(fileName, callback) {
+  let fileSrc = document.getElementById("image-displayed").src;
 
-  if (fileSrc.indexOf(';base64,') !== -1) {
-    fileSrc = fileSrc.replace(/^data:([A-Za-z-+/]+);base64,/, '')
-    fs.writeFile(fileName, fileSrc, 'base64', callback)
+  if (fileSrc.indexOf(";base64,") !== -1) {
+    fileSrc = fileSrc.replace(/^data:([A-Za-z-+/]+);base64,/, "");
+    fs.writeFile(fileName, fileSrc, "base64", callback);
   } else {
-    fileSrc = fileSrc.replace('file://', '')
-    fs.copy(fileSrc, fileName, callback)
+    fileSrc = fileSrc.replace("plp://", "");
+    fs.copy(fileSrc, fileName, callback);
   }
 }
 
 module.exports = {
   applyFilter: applyFilter,
   saveImage: saveImage
-}
+};
