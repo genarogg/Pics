1637f996303bfaa8501a6a0dd8efd5a96a0fc5fe
Genarogg <genarrogg@gmail.com>
2020-03-25 11:24:45 -0400

    Revicion de codigo: Raiz

--- src/devtools.js
+++ src/devtools.js
@@ -1,9 +1,8 @@
+import { enableLiveReload } from "electron-compile";
+import electronDebug from "electron-debug";
 
-import { enableLiveReload } from 'electron-compile'
-import electronDebug from 'electron-debug'
-
-module.exports = function devtools () {
-  enableLiveReload()
+module.exports = function devtools() {
+  enableLiveReload();
   // Mostrando las herramientas de DevTools para las diferentes ventanas
-  electronDebug({ showDevTools: true })
-}
+  electronDebug({ showDevTools: true });
+};
