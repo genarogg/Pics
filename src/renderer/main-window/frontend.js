c703faf693f26e5f435786dec536dc0cc04d50f6
Genarogg <genarrogg@gmail.com>
2020-03-25 12:03:42 -0400

    fix error

--- src/renderer/main-window/frontend.js
+++ src/renderer/main-window/frontend.js
@@ -1,21 +1,34 @@
-import { setIpc, openDirectory, saveFile, openPreferences, uploadImage, pasteImage } from './main-window/ipcRendererEvents'
-import { addImagesEvents, searchImagesEvent, selectEvent, print} from './main-window/images-ui'
+import {
+  setIpc,
+  openDirectory,
+  saveFile,
+  openPreferences,
+  uploadImage,
+  pasteImage
+} from "./main-window/ipcRendererEvents";
+import {
+  addImagesEvents,
+  searchImagesEvent,
+  selectEvent,
+  print
+} from "./main-window/images-ui";
+import createMenu from "./main-window/menu";
 
-window.addEventListener('load', () => {
-  createMenu()
-  setIpc()
-  addImagesEvents()
-  searchImagesEvent()
-  selectEvent()
-  buttonEvent('open-directory', openDirectory)
-  buttonEvent('open-preferences', openPreferences)
-  buttonEvent('save-button', saveFile)
-  buttonEvent('print-button', print)
-  buttonEvent('upload-button', uploadImage)
-  buttonEvent('paste-button', pasteImage)
-})
+window.addEventListener("load", () => {
+  createMenu();
+  setIpc();
+  addImagesEvents();
+  searchImagesEvent();
+  selectEvent();
+  buttonEvent("open-directory", openDirectory);
+  buttonEvent("open-preferences", openPreferences);
+  buttonEvent("save-button", saveFile);
+  buttonEvent("print-button", print);
+  buttonEvent("upload-button", uploadImage);
+  buttonEvent("paste-button", pasteImage);
+});
 
-function buttonEvent (id, func) {
-  const openDirectory = document.getElementById(id)
-  openDirectory.addEventListener('click', func)
+function buttonEvent(id, func) {
+  const openDirectory = document.getElementById(id);
+  openDirectory.addEventListener("click", func);
 }
